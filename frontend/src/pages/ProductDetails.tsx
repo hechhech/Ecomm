e189a46673ddeccf725e8864e1134e496d39589e
hechhech <otayhichem0@gmail.com>
2026-06-16 16:24:56 +0100

    last1

--- frontend/src/pages/ProductDetails.tsx
+++ frontend/src/pages/ProductDetails.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import toast from "react-hot-toast";
-import i18n from "../i18n/config";
 import { productAPI, orderAPI, settingsAPI, Settings, getImageUrl } from "../services/api";
 import { Product, ProductQuantityOffer } from "../types";
 import { formatPrice } from "../utils/formatPrice";
